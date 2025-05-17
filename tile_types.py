92cee8602470b5ff5770869b749e34cd4e2149c0
John Koszarek <john.koszarek@gmail.com>
2025-05-17 17:13:32 -0500

    Comment formatting.

--- tile_types.py
+++ tile_types.py
@@ -6,7 +6,7 @@ import numpy as np  # type: ignore
 graphic_dt = np.dtype(
     [
         ("ch", np.int32),  # Unicode codepoint.
-        ("fg", "3B"),  # 3 unsigned bytes, for RGB colors.
+        ("fg", "3B"),      # 3 unsigned bytes, for RGB colors.
         ("bg", "3B"),
     ]
 )
@@ -14,9 +14,9 @@ graphic_dt = np.dtype(
 # Tile struct used for statically defined tile data.
 tile_dt = np.dtype(
     [
-        ("walkable", bool),  # True if this tile can be walked over.
+        ("walkable", bool),     # True if this tile can be walked over.
         ("transparent", bool),  # True if this tile doesn't block FOV.
-        ("dark", graphic_dt),  # Graphics for when this tile is not in FOV.
+        ("dark", graphic_dt),   # Graphics for when this tile is not in FOV.
         ("light", graphic_dt),  # Graphics for when the tile is in FOV.
     ]
 )
