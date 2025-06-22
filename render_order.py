2c2de711b0e8a17abac511f90e5b08bd83f296e2
John Koszarek <john.koszarek@gmail.com>
2025-06-22 14:09:21 -0500

    Added a comment.

--- render_order.py
+++ render_order.py
@@ -2,6 +2,8 @@ from enum import auto, Enum
 
 
 class RenderOrder(Enum):
+    """Determines the order in which something within a tile is rendered.
+    For example, an Actor is rendered instead of an item or corpse if both occupy the same location."""
     CORPSE = auto()
     ITEM = auto()
     ACTOR = auto()
